8b853ea69fc976f11265741a1ad71b95fd1a12d4
mentino <grazianogiuseppe133@gmail.com>
2026-06-20 10:29:45 +0200

    feat: added player

--- src/app/shared/track/track.component.ts
+++ src/app/shared/track/track.component.ts
@@ -9,28 +9,44 @@ import { AfterViewInit, Component, ElementRef, EventEmitter, Input, Output, View
 export class TrackComponent implements AfterViewInit {
 
     @ViewChild('trackCanvas') trackCanvas?: ElementRef;
+    @ViewChild('hiddenCanvas') hiddenCanvas?: ElementRef;
     @ViewChild('audioFile') audioFile?: ElementRef;
 
     @Output() fileSelected: EventEmitter<{ file: File; data: Float32Array<ArrayBuffer> }> = new EventEmitter();
 
     @Input() color: 'blue' | 'yellow' | 'green' = 'blue';
 
-    trackWidth = window.innerWidth - 25;
-    trackHeight = 200;
+    trackWidth?: number;
+    trackHeight?: number;
+
+    audio: HTMLAudioElement = new Audio();
+
+    drawingBar: any;
+
+    currentFile?: File;
 
     private _canvasContext?: CanvasRenderingContext2D;
 
     constructor() {}
 
     ngAfterViewInit(): void {
+        this.trackWidth = this._pixelToNumber(getComputedStyle(this.trackCanvas?.nativeElement).getPropertyValue('width'));
+        this.trackHeight = this._pixelToNumber(getComputedStyle(this.trackCanvas?.nativeElement).getPropertyValue('height'));
+
         const width = this.trackWidth;
         const height = this.trackHeight;
 
         const canvas = this.trackCanvas?.nativeElement as HTMLCanvasElement;
+        const hiddenCanvas = this.hiddenCanvas?.nativeElement as HTMLCanvasElement;
+
         this._canvasContext = canvas.getContext('2d')!;
+        const hiddenCtx = hiddenCanvas.getContext('2d')!;
 
         canvas.width = width;
         canvas.height = height;
+
+        hiddenCanvas.width = width;
+        hiddenCanvas.height = height;
 
         this._clearCanvas();
 
@@ -41,6 +57,11 @@ export class TrackComponent implements AfterViewInit {
                 return;
             }
 
+            this.currentFile = file;
+
+            const url = URL.createObjectURL(file);
+            this.audio.src = url;
+
             const audioContext = new AudioContext();
 
             const arrayBuffer = await file.arrayBuffer();
@@ -50,16 +71,55 @@ export class TrackComponent implements AfterViewInit {
 
             this.fileSelected.emit({ file, data: channelData });
 
-            const reduced = this._downSample(channelData);
+            const reduced = this._downSample(channelData).filter(r => !!r);
 
             const max = Math.max(...reduced.map(d => Math.abs(d)));
             const maxRatio = height / max;
 
             for (let i = 0; i < reduced.length; i++) {
-                const canvasValue = (Math.abs(reduced[i]) * maxRatio) - 0;
+                const canvasValue = Math.abs(reduced[i]) * maxRatio;
                 this._drawSegment(i, (height - canvasValue) / 2, 1, canvasValue);
             }
+
+            hiddenCtx.drawImage(canvas, 0, 0);
+
+            // Draw bar
+            this.drawingBar = setInterval(() => {
+                this._resetCanvas();
+                this._drawSegment(this._timeToTrack(this.audio.currentTime), 0, 1, max * maxRatio, '#ff0000');
+            }, 1 / 15);
         });
+    }
+
+    private _timeToTrack(time: number) {
+        return time * (this.trackWidth || 1) / this.audio.duration;
+    }
+
+    private _trackToTime(track: number) {}
+
+    toggleAudio() {
+        if (!this.audio.src) {
+            return;
+        }
+
+        if (this.audio.paused) {
+            this.audio.play();
+        } else {
+            this.audio.pause();
+        }
+    }
+
+    stopAudio() {
+        if (!this.audio.src) {
+            return;
+        }
+
+        this.audio.pause();
+        this.audio.currentTime = 0;
+    }
+
+    private _pixelToNumber(p: string) {
+        return +(p.split('p')[0]);
     }
 
     selectAudio() {
@@ -72,7 +132,11 @@ export class TrackComponent implements AfterViewInit {
 
     private _clearCanvas(color = this._getProperty('dark-' + this.color)) {
         this._canvasContext!.fillStyle = color!;
-        this._canvasContext!.fillRect(0, 0, this.trackWidth, this.trackHeight);
+        this._canvasContext!.fillRect(0, 0, this.trackWidth || 0, this.trackHeight || 0);
+    }
+
+    private _resetCanvas() {
+        this._canvasContext?.drawImage(this.hiddenCanvas?.nativeElement as HTMLCanvasElement, 0, 0);
     }
 
     private _drawSegment(x: number, y: number, w: number, h: number, color = this._getProperty(this.color)) {
@@ -82,9 +146,9 @@ export class TrackComponent implements AfterViewInit {
 
     private _downSample(channelData: Float32Array<ArrayBuffer>, targetSize = this.trackWidth) {
         const result = [];
-        const blockSize = channelData.length / targetSize;
+        const blockSize = channelData.length / (targetSize || 1);
 
-        for (let i = 0; i < targetSize; i++) {
+        for (let i = 0; i < (targetSize || 0); i++) {
             const start = Math.floor(i * blockSize);
             const end = Math.floor((i + 1) * blockSize);
 
